e8ca3e5ea1c5dd45c547c6ae44df85205f158825
jammiesymondstayler@gmail.com <jammiesymondstayler@gmail.com>
2020-10-28 16:57:01 +0000

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,12 +1,28 @@
 // .src/containers/MoviesPage.js
-import React from 'react';
-import { Route } from 'react-router-dom';
-import MoviesList from '../components/MoviesList';
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import MoviesList from "../components/MoviesList";
+import MovieShow from "../components/MovieShow";
 
-const MoviesPage = ({ match, movies }) => (
-  <div>
-    <MoviesList movies={movies} />
-  </div>
-)
+const MoviesPage = ({ match, movies }) => {
+    // const logProps = () => {
+    // console.log(match);
+    // console.log(movies);
+    // };
 
-export default MoviesPage
+    // useEffect(logProps);
+
+    return (
+        <div>
+            <MoviesList movies={movies} />
+            <Route
+                path={`${match.url}/:movieId`}
+                render={(routerProps) => (
+                    <MovieShow {...routerProps} movies={movies} />
+                )}
+            />
+        </div>
+    );
+};
+
+export default MoviesPage;
